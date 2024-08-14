c18777c1d2d6fb00586c5378239ec414649cb163
Shakil Ahmed <shakilahmmeed@gmail.com>
2024-08-14 21:24:22 +0600

    Update index.tsx

--- web/src/components/prompts/index.tsx
+++ web/src/components/prompts/index.tsx
@@ -1,11 +1,11 @@
-import AuthModal from "./AuthModal";
-import PostModal from "./PostModal";
+import AuthModal from './AuthModal';
+import PostModal from './PostModal';
 
 export default function Prompts() {
-	return (
-		<>
-			<PostModal />
-			<AuthModal />
-		</>
-	);
+  return (
+    <>
+      <PostModal />
+      <AuthModal />
+    </>
+  );
 }
